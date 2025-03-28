b7c36c5762bb3e738b73015603364b3eb9eb31b8
Jesper Groot <jesperpgroot@gmail.com>
2025-03-28 14:42:50 +0100

    Opdracht 4 van ASP Security

---
+++ S6-ASPSEC-4/Controllers/I0SD3Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace S6_ASPSEC_4.Controllers
+{
+    [Authorize(Roles = "I0SD3")]
+    public class I0SD3Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
